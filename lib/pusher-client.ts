a3e2e823c372417e215d59bf2aec825fc329f224
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2026-05-28 18:55:05 -0400

    testing chat with pusher

--- lib/pusher-client.ts
+++ lib/pusher-client.ts
@@ -2,12 +2,12 @@ import type PusherType from "pusher-js";
 
 let pusherClient: PusherType | null = null;
 
-export function getPusherClient() {
+export function getPusherClient(): PusherType {
     if (!pusherClient) {
         const PusherJS = require("pusher-js");
         pusherClient = new PusherJS(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
             cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-        });
+        }) as PusherType;
     }
-    return pusherClient;
+    return pusherClient!;
 }
